7375b7f6990d2ff871433955f0c3ecd3f2fdcd30
drtonton <tkfoster26@gmail.com>
2016-02-12 12:42:00 -0500

    finished with main assignment(working on optional currently)

--- src/Countries.java
+++ src/Countries.java
@@ -1,11 +1,52 @@
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
 
 public class Countries {
 
+    static HashMap<String, ArrayList<Country>> countryMap = new HashMap<>();
+    static Scanner fileScanner;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        File f = new File("countries.txt");
+        fileScanner = new Scanner(f);
+        ArrayList<Country> countryList = new ArrayList<>();
+
+        while (fileScanner.hasNext()) {
+            String nextLine = fileScanner.nextLine();
+            String [] columns = nextLine.split("\\|");
+            Country country = new Country(columns[0], columns[1]);
+            countryList.add(country);
+        }
+        for (Country tempCountry : countryList) {
+            String firstLetter = String.valueOf(tempCountry.name.charAt(0));
+            countryMap.put(firstLetter, new ArrayList<Country>());
+        }
+        for (Country tempCountry : countryList) {
+            String firstLetter = String.valueOf(tempCountry.name.charAt(0));
+            countryMap.get(firstLetter).add(tempCountry);
+        }
+        System.out.println("Type the first letter of the country you seek:");
+        Scanner scanner = new Scanner(System.in);
+        String nextLine = scanner.nextLine();
+        if (nextLine.length()!=1) {
+            throw new Exception("Invalid Entry: Must be one letter");
+        }
+        ArrayList<Country> tempList = new ArrayList();
+        tempList = countryMap.get(nextLine);
+        String fileContent = new String();
+        for (Country tempCountry : tempList) {
+            fileContent += String.format("%s %s \n", tempCountry.name, tempCountry.abv);
+        }
+        String fileName = nextLine + "_countries.txt";
+        writeFile(fileName, fileContent);
+
+
+
 
     }
 
---
+++ src/Country.java
@@ -0,0 +1,13 @@
+/**
+ * Created by noellemachin on 2/11/16.
+ */
+public class Country {
+
+    String name;
+    String abv;
+
+    public Country (String abv, String name) {
+        this.name = name;
+        this.abv = abv;
+    }
+}
